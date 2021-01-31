ef264e4dd1278ff147c08acbd46d74e59f56a0ca
JevticJovana <65071703+JevticJovana@users.noreply.github.com>
2021-01-31 19:24:33 +0100

    Final

--- hw-4/add_table.php
+++ hw-4/add_table.php
@@ -1,5 +1,6 @@
 <?php
     require 'database_include.php';
+    header('Content-Type: text/html; charset=utf-8');
 
     $movie = $_POST['movie'];
     $decoded = json_decode($movie, true);
@@ -21,7 +22,7 @@
             $movie_notFound = false;
             break;
         }
-        }
+    }
 
     if($movie_notFound) {
         $title = $decoded['Title'];
