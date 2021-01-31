85296453aad30f662f060164ab00802ebf9f4fa4
JevticJovana <65071703+JevticJovana@users.noreply.github.com>
2021-01-31 03:51:40 +0100

    Pretraga po karakteru

--- hw-4/search.php
+++ hw-4/search.php
@@ -5,7 +5,8 @@
     $decoded = json_decode($user, true);
 
     $search = $decoded['Search'];
-    $result = $db->query("SELECT * FROM listmovies WHERE Movie_title = '$search'");
+    // $search = 's';
+    $result = $db->query("SELECT * FROM listmovies WHERE Movie_title LIKE '%$search%'");
     $movie_results = [];
     $counter = 0;
     $length = 0;
@@ -20,10 +21,11 @@
             'Movie_screenwriter' => $row['Movie_screenwriter'], 'Movie_director' => $row['Movie_director'],
             'Movie_studio' => $row['Movie_studio'], 'Movie_actors' => $row['Movie_actors'],
             'Movie_year' => $row['Movie_year'], 'Movie_length' => $row['Movie_length'],
-            'Movie_id' => $row['Movie_id'], 'Movie_poster' => $row['Movie_poster']);
+            'Movie_id' => $row['Movie_id'], 'Movie_poster' => $row['Movie_poster'], 'Movie_grade' => $row['Movie_grade'], 'Grade_counter' => $row['Grade_counter']);
 
+            // echo $movie_results[$counter]['Movie_title'];
             $counter += 1;
-        }
+       }
     }
 
     echo json_encode($movie_results);
